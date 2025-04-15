bc4573eb58aafda96281703e511c989aa9ffe0ba
Prakashjangid429 <157800296+Prakashjangid429@users.noreply.github.com>
2025-04-15 14:18:54 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URI);
+        await mongoose.connect('mongodb+srv://prakashjangid429:LLyAlj0bcUyQoYKr@jdb.epdoitm.mongodb.net/?retryWrites=true&w=majority&appName=jdb');
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
